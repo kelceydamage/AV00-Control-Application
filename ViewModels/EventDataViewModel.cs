f5b8ce19797715240a3ecc831180e832062f683d
kelcey Jamison-Damage <kelcey@kelceydamage.com>
2023-10-04 16:05:49 -0700

    Exploring UI data filtration

--- ViewModels/EventDataViewModel.cs
+++ ViewModels/EventDataViewModel.cs
@@ -4,11 +4,12 @@ using Transport.Messages;
 
 namespace AV00_Control_Application.ViewModels
 {
-
     public class EventDataViewModel
     {
         public ObservableCollection<LogMessage> EventStream { get => eventStream; }
         private readonly ObservableCollection<LogMessage> eventStream;
+
+        public EnumLogMessageType[] EnumLogMessageTypePicker { get => Enum.GetValues<EnumLogMessageType>(); }
 
         public EventDataViewModel()
         {
@@ -18,6 +19,12 @@ namespace AV00_Control_Application.ViewModels
             {
                 eventStream.Add(dummyData);
             }
+        }
+
+        public bool Contains(LogMessage Message)
+        {
+            //LogMessage? message = Message as LogMessage?;
+            return(Message.LogType==EnumLogMessageType.Info);
         }
     } 
 }
