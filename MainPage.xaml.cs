7039b73fabb1bf3e41bcef3df17fc6e6c9b577ee
kelcey Jamison-Damage <kelcey@kelceydamage.com>
2023-10-04 17:37:16 -0700

    Simplify the world

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,28 @@
-﻿namespace AV00_Control_Application
+﻿using AV00_Shared.Logging;
+using System.Collections.ObjectModel;
+using Transport.Messages;
+
+namespace AV00_Control_Application
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        public ObservableCollection<LogMessage> EventStream { get => eventStream; }
+        private readonly ObservableCollection<LogMessage> eventStream;
+        public EnumLogMessageType[] EnumLogMessageTypePicker { get => Enum.GetValues<EnumLogMessageType>(); }
 
         public MainPage()
         {
             InitializeComponent();
+
+            LogMessage dummyData = new(Guid.NewGuid(), "TestService", EnumLogMessageType.Issuing, "This is a test message");
+            eventStream = new() { dummyData };
+            for (var i = 0; i < 20; i++)
+            {
+                eventStream.Add(dummyData);
+            }
+            ObservableCollection<LogMessage> FilteredEventStream = new ObservableCollection<LogMessage>(eventStream);
+            FilteredEventsView.ItemsSource = FilteredEventStream;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -24,6 +40,12 @@
         private void OnLogTypeViewSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Console.WriteLine($"Selection changed {e.CurrentSelection}");
+        }
+
+        public bool Contains(LogMessage Message)
+        {
+            //LogMessage? message = Message as LogMessage?;
+            return (Message.LogType == EnumLogMessageType.Info);
         }
     }
 }
